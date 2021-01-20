5449d709230816823558c0d7848740b97e6cb8ac
Bhushan Patil <bhupatil0001@gmail.com>
2021-01-20 23:14:56 +0530

    Added:solving using case statament used case

--- Empwage.java
+++ Empwage.java
@@ -1,23 +1,27 @@
 public class Empwage {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
-     		System.out.println("Employee Wage Compuatation problem");
-		// constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+		System.out.println("Employee Wage Compuatation problem");
 		// variables
 		int empHrs = 0;
 		int empWage = 0;
 		// computation
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		System.out.println(empCheck);
-		if (empCheck == IS_PART_TIME)
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case IS_PART_TIME:
 			empHrs = 4;
-		else if (empCheck == IS_FULL_TIME)
+			break;
+		case IS_FULL_TIME:
 			empHrs = 8;
-		else
+			break;
+		default:
 			empHrs = 0;
+		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp wage:" + empWage);
 	}
 }
+
