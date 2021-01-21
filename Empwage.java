de8f64c5c50d78ff9faaa69bc9933b182b98ec04
Bhushan Patil <bhupatil0001@gmail.com>
2021-01-22 01:48:57 +0530

    Added:refractor the code use class and method cal wage

--- Empwage.java
+++ Empwage.java
@@ -1,20 +1,19 @@
 public class Empwage {
-	public static final int IS_PART_TIME = 1;
+public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
-		System.out.println("Employee Wage Compuatation problem");
-		// variables
+	// method
+	public void CompanyEmpWage() {
 		int empHrs = 0;
 		int empWage = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
 		int totalEmpWage = 0;
 		// computation
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -36,5 +35,12 @@ public class Empwage {
 		System.out.println("Total Emp HR: " + totalEmpHrs);
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
-}
 
+	public static void main(String[] args) {
+		System.out.println("Employee Wage Compuatation problem");
+
+		Empwage ob = new Empwage();
+		ob.CompanyEmpWage(); //method calling
+	}
+
+}
