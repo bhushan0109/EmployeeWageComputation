d1c7d48a625684d57353a1912222d5dca6f8e248
Bhushan Patil <bhupatil0001@gmail.com>
2021-01-23 19:21:01 +0530

    Added:using OPPs concept count salary multiple company

--- Empwage.java
+++ Empwage.java
@@ -1,46 +1,58 @@
 public class Empwage {
-public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
+	private static final int IS_PART_TIME = 1;
+	private static final int IS_FULL_TIME = 2;
 
-	// method
-	public void CompanyEmpWage() {
-		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-		int totalEmpWage = 0;
-		// computation
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+	private final String company;
+	private final int EMP_RATE_PER_HOUR;
+	private final int MAX_HRS_IN_MONTH;
+	private final int NUM_OF_WORKING_DAYS;
+
+	public Empwage(String company, int EMP_RATE_PER_HOUR, int MAX_HRS_IN_MONTH, int NUM_OF_WORKING_DAYS) 
+	{
+
+		this.company = company;
+		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+		this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+	}
+
+	private int computeEmpWage()
+
+	{
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			switch (empCheck) {
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
+			int attendance = (int) Math.floor(Math.random() * 10) % 3;
+			switch (attendance) {
 			case IS_FULL_TIME:
 				empHrs = 8;
 				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
 			default:
 				empHrs = 0;
-
+				break;
 			}
-
-			System.out.println("Day#:" + totalWorkingDays + "emp Hr " + empHrs);
-			totalEmpHrs = totalEmpHrs + empHrs;
+			totalEmpHrs = (totalEmpHrs + empHrs);
+			System.out.println("Days= " + totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp HR: " + totalEmpHrs);
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+
+		return totalEmpHrs * EMP_RATE_PER_HOUR;
+
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Employee Wage Compuatation problem");
-
-		Empwage ob = new Empwage();
-		ob.CompanyEmpWage(); //method calling
+		System.out.println("Welcome to the Employee Wage Computation program");
+		
+		// created object for company Dmart
+		Empwage dmart = new Empwage("Dmart", 100, 26, 30);
+		System.out.println("Total Employee Wage for Company " + dmart.company + " is: " + dmart.computeEmpWage());
+		
+		// created object for company bigbasket
+		Empwage BigBasket = new Empwage("BigBasket", 150, 23, 25);
+		System.out.println("Total Employee Wage for Company " + BigBasket.company + " is: " + BigBasket.computeEmpWage());
 	}
-
 }
+
